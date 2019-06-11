343d5a3c570c860b78b82f355c32da05fc8ac958
zhangfanlong <izhangfanlong@gmail.com>
2019-06-11 17:18:20 +0800

    add the average evaluate

--- tools/evaluate.m
+++ tools/evaluate.m
@@ -1,10 +1,30 @@
 function [accuracy,sensitivity,specificity,precision,recall,f_measure,gmean,MCC, AUC] = evaluate(PREDICTED, ACTUAL)
+    [label_num_1,accuracy_1,sensitivity_1,specificity_1,precision_1,recall_1,f_measure_1,gmean_1,MCC_1, AUC_1] = evaluate_each(PREDICTED, ACTUAL, 0);
+    [label_num_2,accuracy_2,sensitivity_2,specificity_2,precision_2,recall_2,f_measure_2,gmean_2,MCC_2, AUC_2] = evaluate_each(PREDICTED, ACTUAL, 1);
+    accuracy = average(accuracy_1, label_num_1,accuracy_2, label_num_2);
+    sensitivity = average(sensitivity_1, label_num_1,sensitivity_2, label_num_2);
+    specificity = average(specificity_1, label_num_1,specificity_2, label_num_2);
+    precision = average(precision_1, label_num_1,precision_2, label_num_2);
+    recall = average(recall_1, label_num_1,recall_2, label_num_2);
+    f_measure =average(f_measure_1, label_num_1,f_measure_2, label_num_2);
+    gmean = average(gmean_1, label_num_1,gmean_2, label_num_2);
+    MCC = average(MCC_1, label_num_1,MCC_2, label_num_2);
+    AUC =average(AUC_1, label_num_1,AUC_2, label_num_2);    
+end
 
-idx = (ACTUAL()==1);
+function [average_value] = average(value_1,num_1, value_2, num_2)
+    average_value = (value_1*num_1 +  value_2*num_2)/(num_1+ num_2);
+end
+
+function [label_num,accuracy,sensitivity,specificity,precision,recall,f_measure,gmean,MCC, AUC] = evaluate_each(PREDICTED, ACTUAL, LABEL)
+
+idx = (ACTUAL()==LABEL);
 
 p = length(ACTUAL(idx));
 n = length(ACTUAL(~idx));
 N = p+n;
+
+label_num = p;
 
 tp = sum(ACTUAL(idx)==PREDICTED(idx));
 tn = sum(ACTUAL(~idx)==PREDICTED(~idx));
