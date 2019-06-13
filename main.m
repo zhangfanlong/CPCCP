d6cc91fd84257d755c6fd4958718ffa134d8774d
CirclingEagle <1805662380@qq.com>
2019-06-13 11:28:12 +0800

    封装main

--- main.m
+++ main.m
@@ -1,3 +1,14 @@
+%设置文件路径
+%changingPath = 'D:\Code\Git\CPCCP\data\changing instance';
+%creatingPath = 'D:\Code\Git\CPCCP\data\creating instance';
+%%%Mac文件路径
+changingPath = './data/changing instance';
+creatingPath = './data/creating instance';
+
+entrance(changingPath,creatingPath);
+
+
+function addPath()
 %清空
 clc();
 clear();
@@ -11,18 +22,9 @@ elseif strcmp(filesep, '/')% Mac OS X
     javaaddpath('/Applications/weka-3-6-12/weka.jar')
 end
 javaaddpath([pwd filesep 'matlab2weka' filesep 'matlab2weka' filesep 'matlab2weka.jar']);
+end
 
-%设置文件路径
-%changingPath = 'D:\Code\Git\CPCCP\data\changing instance';
-%creatingPath = 'D:\Code\Git\CPCCP\data\creating instance';
-%%%Mac文件路径
-changingPath = './data/changing instance';
-creatingPath = './data/creating instance';
-
-%初始化，导入数据集
-[changingNames,creatingNames] = ImportData(changingPath,creatingPath);
-
- 
+function start(changingPath,creatingPath,changingNames,creatingNames)
 %调用不同的迁移学习方法进行评估
 disp("Evaluate KMM methods on different classifiers:")
 KMM_evaluate(changingPath,creatingPath,changingNames,creatingNames);
@@ -38,3 +40,12 @@ TCA_evaluate(changingPath,creatingPath,changingNames,creatingNames);
    
 disp("Evaluate JDA methods on different classifiers:")
 JDA_evaluate(changingPath,creatingPath,changingNames,creatingNames);
+end
+
+function entrance(changingPath,creatingPath)
+addPath();
+%初始化，导入数据集
+[changingNames,creatingNames] = ImportData(changingPath,creatingPath);
+%启动
+start(changingPath,creatingPath,changingNames,creatingNames)
+end
